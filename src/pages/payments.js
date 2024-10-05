cb2203ab8f9a1bb7f42b69159c85354d51905d91
jeff <jeffersonking0511@gmail.com>
2024-10-05 09:38:53 +0530

    made few pages responsive

--- src/pages/payments.js
+++ src/pages/payments.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./payments.css"; // Separate CSS for Home
-import qrCode from "../assets/sampleQRCode.png"
+import qrCode from "../assets/sampleQRCode.png";
 
 const bibleVerses = [
   "John 3:16 - For God so loved the world...",
@@ -10,13 +10,13 @@ const bibleVerses = [
   "Proverbs 3:5 - Trust in the Lord with all your heart...",
 ];
 
-const Home = () => {
+const Payments = () => {
   const [currentVerse, setCurrentVerse] = useState(0);
 
   useEffect(() => {
     const verseInterval = setInterval(() => {
       setCurrentVerse((prev) => (prev + 1) % bibleVerses.length);
-    }, 20000); // Change verse every 5 seconds
+    }, 20000); // Change verse every 20 seconds
 
     return () => clearInterval(verseInterval);
   }, []);
@@ -25,33 +25,28 @@ const Home = () => {
     <div className="paymentsPageWrapper">
       <div className="paymentsHeader">
         <h1>Donate</h1>
-        <h2>God loves a <span id="highlight">cheerful</span> giver</h2>
+        <h2>
+          God loves a <span id="highlight">cheerful</span> giver
+        </h2>
       </div>
       <div className="paymentsBody">
         <div className="bankSection">
-            <h3>Bank Details</h3>
-            <br></br>
-            <hr></hr>
-            <br></br>
-            <p>C.S.I. St. Matthew's Church</p>
-            <p>Union Bank</p>
-            <p>Account Number: 12345678xx</p>
-            <p>Selaiyur, East Tambaram</p>
-            <p>Chennai, Tamil Nadu, India</p>
-            <br></br>
+          <h3>Bank Details</h3>
+          <hr />
+          <p>C.S.I. St. Matthew's Church</p>
+          <p>Union Bank</p>
+          <p>Account Number: 12345678xx</p>
+          <p>Selaiyur, East Tambaram</p>
+          <p>Chennai, Tamil Nadu, India</p>
         </div>
         <div className="qrCodeSection">
-            <h3>UPI QR Code</h3>
-            <br></br>
-            <hr></hr>
-            <br></br>
-            <img width="50%" height="70%" src={qrCode} alt="QR Code" />
-            <br></br>
+          <h3>UPI QR Code</h3>
+          <hr />
+          {/* <img src={qrCode} alt="QR Code" /> */}
         </div>
-    </div>
-
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Payments;
